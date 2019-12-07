9b7cfe7e38d44b94adcf66821aea8f191c98368a
helloitsjoe <jboyle617@gmail.com>
2019-12-06 23:46:28 -0500

    day 3

--- day-2.js
+++ day-2.js
@@ -5,6 +5,7 @@ const opMap = {
   1: 'add',
   2: 'multiply',
 }
+
 const intcode = input => {
   let i = 0;
   while (i < input.length) {
@@ -17,11 +18,25 @@ const intcode = input => {
   } 
 }
 
-test(intcode([1,9,10,3,2,3,11,0,99,30,40,50]), [3500,9,10,70,2,3,11,0,99,30,40,50]);
-test(intcode([1,0,0,0,99]), [2,0,0,0,99]);
-test(intcode([2,3,0,3,99]), [2,3,0,6,99]);
-test(intcode([2,4,4,5,99,0]), [2,4,4,5,99,9801]);
-test(intcode([1,1,1,4,99,5,6,0,99]), [30,1,1,4,2,5,6,0,99]);
+for (let i = 0; i <100; i++) {
+  for (let j = 0; j <100; j++) {
+    const dataCopy = [...data];
+    const target = 19690720;
+    dataCopy[1] = i;
+    dataCopy[2] = j;
+    if (intcode(dataCopy)[0] === target) {
+      console.log('Winner!', i, j);
+    }
+  }
+}
 
-console.log(intcode(data));
+
+
+// test(intcode([1,9,10,3,2,3,11,0,99,30,40,50]), [3500,9,10,70,2,3,11,0,99,30,40,50]);
+// test(intcode([1,0,0,0,99]), [2,0,0,0,99]);
+// test(intcode([2,3,0,3,99]), [2,3,0,6,99]);
+// test(intcode([2,4,4,5,99,0]), [2,4,4,5,99,9801]);
+// test(intcode([1,1,1,4,99,5,6,0,99]), [30,1,1,4,2,5,6,0,99]);
+
+// console.log(intcode(data));
 
