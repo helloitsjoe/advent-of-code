f13dd9fd56e6dfa2bfd6d34deaade63209e0a26e
helloitsjoe <jboyle617@gmail.com>
2019-12-12 19:22:15 -0500

    day 3

--- day-3.js
+++ day-3.js
@@ -27,7 +27,7 @@ const getCombinedCoords = commands => {
     if (!combinedCoords.length) return getCoords(command);
     const [lastCoord] = combinedCoords.slice(-1);
     const [x, y] = lastCoord.split(DELIMITER);
-    return [...combinedCoords, ...getCoords(command, { x, y })]
+    return combinedCoords.concat(getCoords(command, { x, y }))
   }, []);
 };
 
@@ -39,7 +39,12 @@ const getCrossPoints = (wire1Coords, wire2Coords) => {
   return wire2Coords.filter(coord => wire1Map[coord]);
 }
 
-const getClosest = crosses => Math.min(...crosses.map(cross => cross.split(DELIMITER).reduce((a, c) => a + Math.abs(Number(c)), 0)));
+const getClosest = crosses => {
+  return crosses.reduce((min, cross) => {
+    const distance = cross.split(DELIMITER).reduce((a, c) => a + Math.abs(Number(c)), 0);
+    return distance < min ? distance : min;
+  }, Infinity)
+}
 
 console.clear();
 // test(getCoords('R3'), ['1,0', '2,0', '3,0'], 'RIGHT coord test');
@@ -50,14 +55,13 @@ console.clear();
 // test(getCombinedCoords(['R3', 'U2']), ['1,0', '2,0', '3,0', '3,1', '3,2'], 'combines coords');
 // test(getCrossPoints(getCombinedCoords(['R8','U5','L5','D3']), getCombinedCoords(['U7','R6','D4','L4'])), ['6,5', '3,3'], 'gets cross points');
 // test(getClosest(['6,5', '3,3']), 6, 'gets distance');
-test(getClosest(['6,5', '3,-12']), 11, 'gets distance with negative');
+// test(getClosest(['6,5', '3,-12']), 11, 'gets distance with negative');
 
 const manhattan = (wire1, wire2) => {
   const crosses = getCrossPoints(getCombinedCoords(wire1), getCombinedCoords(wire2));
-  console.log(getClosest(crosses));
   return getClosest(crosses);
 };
 
-test(manhattan(['R75','D30','R83','U83','L12','D49','R71','U7','L72'],['U62','R66','U55','R34','D71','R55','D58','R83']), 159, 'first test');
-test(manhattan(['R98','U47','R26','D63','R33','U87','L62','D20','R33','U53','R51'],['U98','R91','D20','R16','D67','R40','U7','R15','U6','R7']), 135, 'second test');
+// test(manhattan(['R75','D30','R83','U83','L12','D49','R71','U7','L72'],['U62','R66','U55','R34','D71','R55','D58','R83']), 159, 'first test');
+// test(manhattan(['R98','U47','R26','D63','R33','U87','L62','D20','R33','U53','R51'],['U98','R91','D20','R16','D67','R40','U7','R15','U6','R7']), 135, 'second test');
 test(manhattan(data[0], data[1]), 446, 'gets actual answer')
