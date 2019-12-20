0253c80a0b7b01428e6d23a7c785b6b8be246507
helloitsjoe <jboyle617@gmail.com>
2019-12-19 21:38:58 -0500

    day 6

--- day-6.js
+++ day-6.js
@@ -1,6 +1,15 @@
 const data = require('./day-6-data');
 const test = require('./test');
 
+const convertToMap = arr => {
+  return arr.reduce((acc, c) => {
+    const [host, orbiter] = c.split(')');
+    acc[host] = orbiter;
+    return acc;
+  }, {});
+};
+
+
 const getRoot = input => {
   const orbiters = input.reduce((acc, c) => {
     const [a, b] = c.split(')');
@@ -15,6 +24,14 @@ const getRoot = input => {
   });
   return roots.filter(Boolean);
 }
+
+const getNumOrbits = root => {
+  
+}
+
+const dataMap = convertToMap(data);
+
+console.log(dataMap);
 
 test(getRoot(data), 'COM', 'root is COM');
 
