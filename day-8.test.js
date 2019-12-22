5f232022d5a477156ef4983c86c5a18402e88c5c
helloitsjoe <jboyle617@gmail.com>
2019-12-22 09:06:28 -0500

    day 8 part 2

--- day-8.test.js
+++ day-8.test.js
@@ -4,6 +4,10 @@ const WIDTH = 25;
 const HEIGHT = 6;
 const LAYER_SIZE = WIDTH * HEIGHT;
 
+const BLACK = 0;
+const WHITE = 1;
+const TRANS = 2;
+
 const getLayers = data => {
   const layers = [];
 
@@ -49,3 +53,31 @@ it('finds ones multiplied by twos', () => {
   const onesByTwos = getOnesByTwos(layers[fewestIndex]);
   expect(onesByTwos).toBe(2440);
 });
+
+const getPattern = (str, layerSize = LAYER_SIZE) => {
+  // 25 Width, 6 height, 100 layers means we need to jump by 100
+  // starting at the first position. When we reach the last position,
+  // record final transparency.
+  const finalLayer = Array(layerSize).fill('');
+
+  for (let i = 0; i < layerSize; i++) {
+    for (let j = i; j < str.length; j += layerSize) {
+      if (finalLayer[i]) break;
+
+      if (str[j] !== '2') {
+        // console.log('str[j]', str[j])
+        finalLayer[i] = str[j];
+      }
+    }
+  }
+  return finalLayer;
+}
+
+const makeRows = () => {};
+
+it('gets simple pattern', () => {
+  const actual = getPattern('2222 1202 0011 0000'.replace(/ /g, ''), 4);
+  expect(actual).toEqual(['1', '0', '0', '1']);
+});
+
+
