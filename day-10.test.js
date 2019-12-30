4f85a14b9a64e1a18de2a6d6bee8384888a6283e
helloitsjoe <jboyle617@gmail.com>
2019-12-29 22:26:52 -0500

    Trying to fix day 10

--- day-10.test.js
+++ day-10.test.js
@@ -2,8 +2,23 @@
 
 // console.log(data);
 
+const convertToArr = input => input.map(ea => ea[0].split(''));
+
+const getAllCounts = input => {
+  const arrInput = convertToArr(input);
+  const output = arrInput.map(ea => Array(arrInput[0].length).fill(0));
+
+  for (let y = 0; y < arrInput.length; y++) {
+    const row = arrInput[y];
+    for (let x = 0; x < row.length; x++) {
+      output[y][x] = getCount(x, y, arrInput);
+    }
+  }
+  return output;
+};
+
 const getCount = (x, y, arr) => {
-  const miss = (row, x) => row && row[x] !== '#';
+  const miss = (row, x) => !row || row && row[x] !== '#';
   const hit = (row, x) => row && row[x] === '#';
 
   let count = 0;
@@ -42,45 +57,94 @@ const getCount = (x, y, arr) => {
   for (let direction of directions) {
     const { opX, opY } = direction;
     const length = Math.max(arr.length, row.length);
-
-    for (let xOffset = 1; xOffset < length; xOffset++) {
-      let found = false;
-      for (let yOffset = 1; yOffset < length; yOffset++) {
+    
+    const find = (xOffset, yOffset) => {
         for (let range = 1; range < length; range++) {
           const xWithRange = operate(x, opX, range * xOffset);
           const yWithRange = operate(y, opY, range * yOffset);
 
+          if (miss(arr[yWithRange], xWithRange)) break;
+
+          if (found) {
+            console.log('found:', xWithRange, yWithRange, '| xOff:', xOffset, 'yOff:', yOffset)
+            break;
+          }
+
           if (hit(arr[yWithRange], xWithRange)) {
+            console.log('HIT:', xWithRange, yWithRange, '| xOff:', xOffset, 'yOff:', yOffset)
             count++;
             found = true;
             break;
           }
         }
-        if (found) break;
+    }
+
+    let found = false;
+
+    for (let xOffset = 1; xOffset < length; xOffset++) {
+      for (let yOffset = 1; yOffset < length; yOffset++) {
+        find(xOffset, yOffset);
       }
-      if (found) break;
     }
   }
   
   return count;
 };
 
-const convertToArr = input => input.map(ea => ea[0].split(''));
-
-const getAllCounts = input => {
+fit('minimal directional example', () => {
+  // My algorithm needs fixing. It hits on 4,2 | 2,4 | 3,3
+  // but misses on 3,4 and 4,3
+  const input = [
+    ['.....'],
+    ['.....'],
+    ['..#.#'],
+    ['...##'],
+    ['..###']
+  ];
   const arrInput = convertToArr(input);
-  const output = arrInput.map(ea => Array(arrInput[0].length).fill(0));
+  expect(getCount(2, 2, arrInput)).toBe(5);
+});
 
-  for (let y = 0; y < arrInput.length; y++) {
-    const row = arrInput[y];
-    for (let x = 0; x < row.length; x++) {
-      output[y][x] = getCount(x, y, arrInput);
-    }
-  }
-  return output;
-};
 
-it('small test', () => {
+it('getCount detects asteroids vertically', () => {
+  const input = [
+    ['#..'],
+    ['#..'],
+    ['#..']
+  ]
+  const arrInput = convertToArr(input);
+  expect(getCount(0, 0, arrInput)).toBe(1);
+  expect(getCount(0, 1, arrInput)).toBe(2);
+  expect(getCount(0, 2, arrInput)).toBe(1);
+});
+
+it('finds in every direction', () => {
+  const input = [
+    ['#####'],
+    ['#...#'],
+    ['#.#.#'],
+    ['#...#'],
+    ['#####']
+  ];
+  const arrInput = convertToArr(input);
+  expect(getCount(2, 2, arrInput)).toBe(16);
+});
+
+it('getCount detects asteroids horizontally with a space', () => {
+  const input = [
+    ['.....'],
+    ['#.#.#'],
+  ]
+  const arrInput = convertToArr(input);
+  expect(getCount(0, 0, arrInput)).toBe(0);
+  expect(getCount(0, 1, arrInput)).toBe(1);
+  expect(getCount(1, 1, arrInput)).toBe(0);
+  expect(getCount(2, 1, arrInput)).toBe(2);
+  expect(getCount(3, 1, arrInput)).toBe(0);
+  expect(getCount(4, 1, arrInput)).toBe(1);
+});
+
+xit('small test', () => {
   const input = [
     ['.#..#'],
     ['.....'],
@@ -139,32 +203,6 @@ it('getCount detects asteroids vertically with a space', () => {
   expect(getCount(0, 2, arrInput)).toBe(2);
   expect(getCount(0, 3, arrInput)).toBe(0);
   expect(getCount(0, 4, arrInput)).toBe(1);
-});
-
-it('getCount detects asteroids horizontally with a space', () => {
-  const input = [
-    ['.....'],
-    ['#.#.#'],
-  ]
-  const arrInput = convertToArr(input);
-  expect(getCount(0, 0, arrInput)).toBe(0);
-  expect(getCount(0, 1, arrInput)).toBe(1);
-  expect(getCount(1, 1, arrInput)).toBe(0);
-  expect(getCount(2, 1, arrInput)).toBe(2);
-  expect(getCount(3, 1, arrInput)).toBe(0);
-  expect(getCount(4, 1, arrInput)).toBe(1);
-});
-
-it('getCount detects asteroids vertically', () => {
-  const input = [
-    ['#..'],
-    ['#..'],
-    ['#..']
-  ]
-  const arrInput = convertToArr(input);
-  expect(getCount(0, 0, arrInput)).toBe(1);
-  expect(getCount(0, 1, arrInput)).toBe(2);
-  expect(getCount(0, 1, arrInput)).toBe(2);
 });
 
 
